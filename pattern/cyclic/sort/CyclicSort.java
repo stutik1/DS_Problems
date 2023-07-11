8e791fd1d0d8057cd458eff2958ef5f2c47a365a
megha <stutimegha2019@gmail.com>
2023-07-11 14:31:29 +0530

    using cyclic sort and sorting method

---
+++ pattern/cyclic/sort/CyclicSort.java
@@ -0,0 +1,35 @@
+package pattern.cyclic.sort;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if(i != arr[i]-1){
+                int index= arr[i]-1;
+                int x = arr[i];
+                arr[i] = arr[index];
+                arr[index] = x;
+            }else{
+                i++;
+            }
+        }
+    }
+
+    public static int[] bySorting(int[] arr){
+        Arrays.sort(arr);
+        return arr;
+    }
+    public static void main(String[] args){
+        int[] arr ={3, 4, 6, 2, 1, 5};
+
+        System.out.println(Arrays.toString(bySorting(arr)));
+
+        CyclicSort.cyclicSort(arr);
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
+
