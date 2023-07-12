4a92a50114cf41649c96ec379a1f7e804dfee744
megha <stutimegha2019@gmail.com>
2023-07-12 17:32:19 +0530

    Add two different approaches

--- pattern/cyclic/sort/MissingNumber.java
+++ pattern/cyclic/sort/MissingNumber.java
@@ -1,7 +1,31 @@
 package pattern.cyclic.sort;
 
 public class MissingNumber {
-    public static int missingNumber(int[] arr) {
+
+    public static int missingNumberApproach1(int[] arr) {
+        int n = arr.length + 1;
+
+        int sum = (n * (n + 1)) / 2;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum - arr[i];
+        }
+        return sum;
+    }
+
+    public static int missingNumberApproach2(int[] arr) {
+        int n = arr.length;
+        int result = 0;
+        int totalSum = 0;
+
+        int sum = (n + 1) * (n + 2) / 2;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum = totalSum + arr[i];
+            result = sum - totalSum;
+        }
+        return result;
+    }
+
+    public static int missingNumberCyclicSort(int[] arr) {
         int i = 0;
         while (i < arr.length) {
             if (arr[i] != i + 1 && arr[i] <= arr.length) {
@@ -24,6 +48,11 @@ public class MissingNumber {
 
     public static void main(String[] args) {
         int[] arr = {3, 4, 6, 2, 1, 7};
-        System.out.println(MissingNumber.missingNumber(arr));
+        System.out.println(MissingNumber.missingNumberApproach1(arr));
+
+        System.out.println(MissingNumber.missingNumberApproach2(new int[]{3, 4, 6, 2, 5, 7, 8}));
+
+        System.out.println(MissingNumber.missingNumberCyclicSort(new int[]{3, 4, 6, 2, 1, 7}));
     }
+
 }
