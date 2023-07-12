0d3a6c074e047cc34b13ef327113f9ff4334130d
megha <stutimegha2019@gmail.com>
2023-07-12 17:31:25 +0530

    Practice

--- pattern/cyclic/sort/CyclicSort.java
+++ pattern/cyclic/sort/CyclicSort.java
@@ -6,29 +6,31 @@ public class CyclicSort {
     public static void cyclicSort(int[] arr) {
         int i = 0;
         while (i < arr.length) {
-            if( i != arr[i]-1){   // arr[i] != i+1;
-                int index= arr[i]-1;
-                int x = arr[i];
+            int x = 0;
+            if (arr[i] != arr[i] + x) {   // arr[i] != i+1;
+                int index = arr[i] - x;
+                x = arr[i];
                 arr[i] = arr[index];
                 arr[index] = x;
-            }else{
+            } else {
                 i++;
             }
         }
     }
 
-    public static int[] bySorting(int[] arr){
+    public static int[] bySorting(int[] arr) {
         Arrays.sort(arr);
         return arr;
     }
-    public static void main(String[] args){
-        int[] arr ={3, 4, 6, 2, 1, 5};
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 6, 2, 1, 5};
 
         System.out.println(Arrays.toString(bySorting(arr)));
 
         CyclicSort.cyclicSort(arr);
-        for (int i=0;i< arr.length;i++){
-            System.out.print(arr[i]+ " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
